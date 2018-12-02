5ffd1d7809290d5a155fef8a663bb74cbcaa84ec
truongnnd00546 <truongnnd00546@fpt.edu.vn>
2018-12-03 02:23:34 +0700

    hoan-thanh

---
+++ ASP.NETMVC5withLambdaExpressions/Context/BookContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using ASP.NETMVC5withLambdaExpressions.Models;
+
+namespace ASP.NETMVC5withLambdaExpressions.Context
+{
+    public class BookContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ ASP.NETMVC5withLambdaExpressions/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP.NETMVC5withLambdaExpressions.MvcApplication" Language="C#" %>
